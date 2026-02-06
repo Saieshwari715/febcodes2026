3492fbf36a0d6344e87c1da3acbd0bc59c88d0ed
Saieshwari715 <gajulasaieshwari0131@gmail.com>
2026-02-06 11:44:07 +0530

    Time: 0 ms (100%), Space: 19.3 MB (47.71%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,34 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=2:
+            return n
+        a=1
+        b=2
+        for i in range(3,n+1):
+            c=a+b
+            a=b
+            b=c
+        return b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''if n <= 2:
+            return n
+        a, b = 1, 2 
+        for i in range(3, n+1):
+            a, b = b, a + b
+        return b'''
+
+        
